938327af6e7c2da449e965f5a42abd7fd85f7347
Cale <ck@patsoffroad.com>
2017-10-24 20:16:13 -0500

    Create google_script.js

---
+++ google_script.js
@@ -0,0 +1,23 @@
+function doGet(e) {
+  if(typeof e !== 'undefined')
+  Console.log("get " + JSON.stringify(e.parameter));
+  return ContentService.createTextOutput(JSON.stringify(e.parameter));
+}
+
+function doPost(e) {
+  if(typeof e !== 'undefined')
+    Console.log(e.postData.contents)
+    Console.log("post data stringify "+JSON.stringify(e.postData.contents));
+    Console.log("post query " + JSON.stringify(e.parameter));
+    return ContentService.createTextOutput(JSON.stringify(e.parameter));
+}
+
+var SSID = "google_sheet_key"
+//create a sheet named Console for console logging
+var Console = new Object;
+Console.log = function(e){
+  var sheet = SpreadsheetApp.openById(SSID).getSheetByName("Console");
+  sheet.insertRowBefore(2);
+  var data = [[new Date, e]];
+  sheet.getRange(2,1,1,2).setValues(data);
+}
